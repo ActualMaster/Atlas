20b5e3fa0e3605199315512e4025699d6cebf115
RealMaster <119766152+ActualMaster@users.noreply.github.com>
2023-02-11 20:42:50 -0600

    Update and rename Keys&Blacklist to Keys&Blacklist.lua

---
+++ Keys&Blacklist.lua
@@ -0,0 +1,9 @@
+local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
+
+keys = {
+  ["ENHzuaN8D5mqR875wGkafKAhXjLfapI8WLLHBJrGpRSobQfMENfiCZRLZNDZwJYSaQWODbPkiu1ftWF1zEBVCqhflqzgIhtAL3BP"] = true
+}
+
+local blacklist = {
+  ["00000000-0000-0000-0000-000000000000"] = true
+}
