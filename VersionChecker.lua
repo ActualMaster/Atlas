bb7d8e236451b4a22018a773b09b7dd54fcf1914
RealMaster <119766152+ActualMaster@users.noreply.github.com>
2023-02-15 20:40:07 -0600

    Update VersionChecker.lua

--- VersionChecker.lua
+++ VersionChecker.lua
@@ -1,18 +1,26 @@
 local Atlas = {}
 local default = 'Latest'
 
-local Atlas.Versions = {
-  'Latest'
+Atlas.Version = default
+
+Atlas.Versions = {
+  'Latest',
   'V1'
 }
 
-if not Atlas.Version == 'Latest' then
-  return 'Version Not Latest'
+local function NotLatest()
+    if Atlas.Version ~= 'Latest' then
+      return 'Version Not Latest'
+    end
 end
+
+local notlatest = NotLatest()
+print(notlatest)
 
 local function getversion()
   version = Atlas.Version
   return version
 end
 
-getversion()
+local word = getversion()
+print(word)
