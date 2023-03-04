16f68a4b672af5e8983e6ad1620485668d57fb53
RealMaster <119766152+ActualMaster@users.noreply.github.com>
2023-03-04 14:11:20 -0600

    Update Launch.lua

--- Launch.lua
+++ Launch.lua
@@ -1,1 +1,5 @@
-
+--Impossible Ladder Climb
+if game.PlaceId == 12019414465 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ActualMaster/Atlas/main/Games/ImpossibleLadderClimb.lua", true))()
+    print('Loaded Atlas For 12019414465')
+end
